c88abe656cf0883e4e3ac2e69b635f0ed3a2f066
K.Kiyokawa <koichi20110068@gmail.com>
2020-01-10 15:52:54 +0900

    Run fisher after sccessing install fisher

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ mkdir -p ~/.config
 ln -fs ~/dotfiles/fish ~/.config
 ln -fs ~/dotfiles/nvim ~/.config
 ln -fs ~/dotfiles/.tigrc ~
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish &&
 fisher
