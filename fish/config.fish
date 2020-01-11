196265ba3a96eb393b4931e7a896ed45cfa54784
K.Kiyokawa <koichi20110068@gmail.com>
2020-01-11 15:46:15 +0900

    Extract env var

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,7 @@
 # ------------------------------------------------------------------------------
 # 環境変数
 # ------------------------------------------------------------------------------
-# Python
-# set -x  PYENV_ROOT $HOME/.pyenv
-# set -x  PATH $PYENV_ROOT/shims $PATH
-# eval (pyenv init - | source)
-
-# Ruby
-status --is-interactive; and source (rbenv init -|psub)
-set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
-
-# Java
-set -x JAVA_HOME (/usr/libexec/java_home  -v 1.8)
-set -x PATH $JAVA_HOME/bin $PATH
-
-# Go
-set -x GOPATH $HOME/prg/go
-
-# Flutter
-set -x PATH $HOME/flutter/bin $PATH
+source ~/.config/fish/env.fish
 
 # ------------------------------------------------------------------------------
 # エイリアス
---
+++ fish/env.fish
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------
+# 環境変数
+# ------------------------------------------------------------------------------
+
+# Python
+# set -x  PYENV_ROOT $HOME/.pyenv
+# set -x  PATH $PYENV_ROOT/shims $PATH
+# eval (pyenv init - | source)
+
+# Ruby
+status --is-interactive; and source (rbenv init -|psub)
+set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
+
+# Java
+set -x JAVA_HOME (/usr/libexec/java_home  -v 1.8)
+set -x PATH $JAVA_HOME/bin $PATH
+
+# Go
+set -x GOPATH $HOME/prg/go
+
+# Flutter
+set -x PATH $HOME/flutter/bin $PATH
