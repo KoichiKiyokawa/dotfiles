34e9c1e4cba6afda355d6c535fed08d5334f1588
K.Kiyokawa <koichi20110068@gmail.com>
2020-04-07 00:00:45 +0900

    Delete tig settings

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ mkdir -p ~/.config
 rm -rf ~/.config/fish
 ln -fs ~/dotfiles/fish ~/.config
 ln -fs ~/dotfiles/nvim ~/.config
-ln -fs ~/dotfiles/.tigrc ~
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 nvim -c "PlugInstall | :qa"
