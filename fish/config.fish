1b0098b2435d6b8db7fa8e5eeeec552b561a1d54
K.Kiyokawa <koichi20110068@gmail.com>
2021-12-21 12:25:49 +0900

    Add pnpm completion

--- fish/config.fish
+++ fish/config.fish
@@ -29,3 +29,7 @@ set EDITOR nvim
 
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
