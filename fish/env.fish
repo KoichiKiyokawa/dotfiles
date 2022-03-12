433cb3233a4292b6d59176e06bd969838b107ea9
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-03-12 13:23:45 +0900

    chore: comment out pyenv settings

--- fish/env.fish
+++ fish/env.fish
@@ -6,10 +6,10 @@
 eval (/opt/homebrew/bin/brew shellenv)
 
 # Python
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-status is-login; and pyenv init --path | source
-pyenv init - | source
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+# status is-login; and pyenv init --path | source
+# pyenv init - | source
 
 # Ruby
 rvm default
