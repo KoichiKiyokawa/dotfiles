8fd77ff4a69184b282d0e0c287cd0e75797dc59c
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-04-08 19:31:01 +0900

    fix: synbolic link for karabiner
    * https://karabiner-elements.pqrs.org/docs/manual/misc/configuration-file-path/

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.config
 rm -rf ~/.config/fish
 ln -fs ~/dotfiles/fish ~/.config
 ln -fs ~/dotfiles/nvim ~/.config
-ln -fs ~/dotfiles/karabiner.json ~/.config/karabiner
+ln -fs ~/dotfiles/karabiner ~/.config/karabiner
 ln -fs ~/dotfiles/vscode/.vimrc ~/.vimrc
 ln -fs ~/dotfiles/.gitconfig ~/.gitconfig
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
