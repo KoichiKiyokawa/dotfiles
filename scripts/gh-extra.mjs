f9aad2b5bdc0bac6cbfaba1aa94216489aa14e7b
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-10-18 10:15:55 +0900

    refactor: use `process.chdir`

--- scripts/gh-extra.mjs
+++ scripts/gh-extra.mjs
@@ -5,7 +5,7 @@ import { parseGitHubURL } from "./utils/parse-url.mjs"
 const { 2: action, 3: targetPullRequestURL } = process.argv
 const { repo, prNo } = parseGitHubURL(targetPullRequestURL)
 
-const cwd = repoToPath(repo)
+process.chdir(repoToPath(repo))
 if (action === "open") {
   execSync(`gh pr checkout ${prNo} && code .`, { cwd })
 } else if (action === "approve") {
