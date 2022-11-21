6b50448bbd77e002d8a760b8a959f6fa37cd2928
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-11-21 23:03:32 +0900

    feat: move gh-extra.mjs to gh-extra.sh

---
+++ scripts/gh-extra.sh
@@ -0,0 +1,21 @@
+cd "$(dirname "$0")"
+. ./env.sh
+
+action=$1
+targetPullRequestURL=$2
+parsed=($(echo $targetPullRequestURL | perl -pe 's|.+?com/.+?/(.+?)/pull/(\d+)|$1 $2|'))
+repo=${parsed[0]}
+prNo=${parsed[1]}
+
+cdToRepoPath $repo
+
+changedFileCount=`git status --short | wc -l`
+if [ $changedFileCount -gt 0 ]; then
+  echo "\n[ERROR] There are uncommitted changes in the repo. Please stash or commit them first.\n"
+  exit 1
+fi
+
+case $action in
+  "open") gh pr checkout $prNo && code . ;;
+  "approve") gh pr review -a $prNo ;;
+esac
