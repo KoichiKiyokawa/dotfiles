3e605a7ad80e5d5572073a443ef38358b2da0e02
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-11-21 14:38:39 +0900

    feat: check diff existence

--- scripts/gh-extra.mjs
+++ scripts/gh-extra.mjs
@@ -6,6 +6,16 @@ const { 2: action, 3: targetPullRequestURL } = process.argv
 const { repo, prNo } = parseGitHubURL(targetPullRequestURL)
 
 process.chdir(repoToPath(repo))
+
+const response = execSync(`git status --short | wc -l`)
+const changedFileCount = Number(response.toString().trim())
+if (changedFileCount > 0) {
+  console.log(
+    "\nThere are uncommitted changes in the repo. Please stash or commit them first.\n"
+  )
+  process.exit(1)
+}
+
 if (action === "open") {
   execSync(`gh pr checkout ${prNo} && code .`)
 } else if (action === "approve") {
