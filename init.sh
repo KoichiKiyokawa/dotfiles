98c1b98ce1045d14d3420140c036300fcb15c989
Koichi Kiyokawa <koichi20110068@gmail.com>
2024-12-14 00:01:27 +0900

    chore: local.zsh

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ ln -fs ~/dotfiles/nvim ~/.config
 ln -fs ~/dotfiles/karabiner ~/.config
 
 ln -fs ~/dotfiles/zsh/.zshrc ~/.zshrc
+touch ~/dotfiles/zsh/local.zsh
 mkdir -p ~/.config/sheldon
 ln -fs ~/dotfiles/zsh/plugins.toml ~/.config/sheldon/plugins.toml
 ln -fs ~/dotfiles/zsh/starship.toml ~/.config/starship.toml
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,3 +2,4 @@ eval "$(starship init zsh)"
 eval "$(sheldon source)"
 eval "$(fnm env --use-on-cd --shell zsh)"
 zsh-defer source ~/dotfiles/zsh/alias.zsh
+zsh-defer source ~/dotfiles/zsh/local.zsh
